64e37e4344e9cdbf0dea8d6ebf08f5106f96bbf2
Zuleika <zuleika4097@atomicmail.io>
2025-07-29 15:01:28 +0200

    Add base settings management

--- main.py
+++ main.py
@@ -1,5 +1,56 @@
+import pydantic
+from rich import print
+from rich.panel import Panel
+from rich.prompt import Prompt
+
+from book_manager.config import Settings
+
+
+def prompt_credentials():
+    print(
+        Panel(
+            "Instructions for obtaining the tokens can be found "
+            "[link=https://youtu.be/X4msqCulOYk]here[/link].",
+            title="Note",
+            style="gray50",
+        )
+    )
+    book_id = Prompt.ask("Enter your book ID")
+    try:
+        book_id = int(book_id)
+    except ValueError:
+        print("[bold red]Error[/bold red]: book ID must be a valid integer.")
+        exit(1)
+
+    auth_token = Prompt.ask("Enter your authentication token")
+    recaptcha_token = Prompt.ask("Enter your recaptcha token")
+    create_config = Prompt.ask(
+        r"Do you want to create a config file? [bold]\[yes/no][/bold]"
+    )
+    if create_config == "yes":
+        with open(".env", "w", encoding="utf-8") as config_file:
+            config_file.writelines(
+                [
+                    f"BOOK_ID={book_id}\n",
+                    f"AUTH_token={auth_token}\n",
+                    f"RECAPTCHA_TOKEN={recaptcha_token}\n",
+                ]
+            )
+
+    return Settings(
+        book_id=book_id, auth_token=auth_token, recaptcha_token=recaptcha_token
+    )
+
+
 def main():
-    print("Hello from book-manager!")
+    print(":books:[bold]Book Manager[/bold] starting...")
+
+    try:
+        config = Settings()
+    except pydantic.ValidationError:
+        config = prompt_credentials()
+
+    print(config)
 
 
 if __name__ == "__main__":
